001662eb88e38ed593bdc358a045b5089cfa2697
skillitt <markdevore@outlook.com>
2024-03-28 11:19:23 -0400

    still working on it

--- cipher.py
+++ cipher.py
@@ -1,4 +1,14 @@
 # create alphabet dictionary
+def create_caesar_cipher_dict(shift):
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    caesar_dict = {}
+    for i in range(len(alphabet)):
+        caesar_dict[alphabet[i]] = alphabet[(i + shift) % len(alphabet)]
+    return caesar_dict
+
+caesar_dict_shift_5 = create_caesar_cipher_dict(5)
+
+print(caesar_dict_shift_5)
 
 
 
@@ -9,6 +19,18 @@
     #- Output: shifted letter
     #- Shift the letter by the specified amount, considering wrapping around the alphabet
 
+def shift_letter(letter, shift_amount):
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    if letter in alphabet:
+        shifted_index = (alphabet.index(letter) + shift_amount) % 26
+        shifted_letter = alphabet[shifted_index]
+        return shifted_letter
+    else:
+        return letter 
+    
+    original_letter = 'a'
+shifted_letter = shift_letter(original_letter, 5)
+print(shifted_letter)
 
 
 #Create a function to encrypt a message using the Caesar cipher
